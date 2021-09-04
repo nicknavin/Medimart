b586b8f53a7428a764079bb5840151646cc7d2f9
nicknavin <nimade.navin@gmail.com>
2021-09-04 14:10:11 +0530

    Initial commit

---
+++ app/src/main/java/com/app/medimart/ui/main/view/HomeActivity.kt
@@ -0,0 +1,12 @@
+package com.app.medimart.ui.main.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.app.medimart.R
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
